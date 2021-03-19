5b9bcb0ddddddf3f3bbfa10317a40b11ca5bf9fc
Brent Besase <bbesase51@gmail.com>
2021-03-19 15:31:45 -0400

    feature/about-page
    Got some basic styling done for the page as well as filled out some content. Picked a set of fonts

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,32 @@
 <svelte:head>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Lato&family=Oswald&display=swap" rel="stylesheet">
+
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<p class="header">Heya :) I'm Brent.</p>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p class="sub-header">I use my skillz to build things for the largest source of information, the web!</p>
+
+<p class="about">I am a Software Engineer that works on medical applications to improve a patients time inside a hospital room that are built on Angular.</p>
+
+<style>
+	.header {
+		font-size: 50px;
+		font-family: 'Indie Flower', cursive;
+		font-weight: 400;
+	}
+
+	.sub-header {
+		font-size: 40px;
+		font-family: 'Oswald', sans-serif;
+		font-weight: 400;
+		text-transform: uppercase;
+	}
+
+	.about {
+		font-size: 20px;
+		font-family: 'Lato', sans-serif;
+	}
+</style>
