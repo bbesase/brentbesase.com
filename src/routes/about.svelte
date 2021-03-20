4c343e43782b73018417e22c4415717bea7ca373
Brent Besase <bbesase51@gmail.com>
2021-03-20 11:22:15 -0400

    feature/theme
    Switched the about section with the home section

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,35 +5,12 @@
 	<title>About</title>
 </svelte:head>
 
-<p class="header">Heya :) I'm Brent.</p>
-
-<p class="sub-header">I use my skillz to build things for the piece of technology that everybody uses... the web!</p>
-
-<p class="tiny">*Insert evil laugh here*</p>
-
-<p class="about">I am a Software Engineer that works on medical applications to improve a patients time inside a hospital room that are built on Angular.</p>
+<p class="header">About Me</p>
 
 <style>
 	.header {
 		font-size: 50px;
 		font-family: 'Indie Flower', cursive;
 		font-weight: 400;
-	}
-
-	.sub-header {
-		font-size: 40px;
-		font-family: 'Oswald', sans-serif;
-		font-weight: 400;
-		text-transform: uppercase;
-	}
-
-	.tiny {
-		font-size: 20px;
-		font-family: 'Oswald', sans-serif;
-	}
-
-	.about {
-		font-size: 25px;
-		font-family: 'Lato', sans-serif;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
-
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Lato&family=Oswald&display=swap" rel="stylesheet">
@@ -9,14 +5,41 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<p class="header">Home Text</p>
+<p class="header">Heya :) I'm Brent.</p>
+
+<p class="sub-header">I use my skillz to build things for the piece of technology that everybody uses... the web!</p>
+
+<p class="tiny">*Insert evil laugh here*</p>
+
+<p class="about">I am a Software Engineer that works on medical applications to improve a patients time inside a hospital room that are built on <span class ="color">Angular.</span></p>
 
 <style>
 	.header {
 		font-size: 50px;
 		font-family: 'Indie Flower', cursive;
 		font-weight: 400;
-		color: rgb(237,237,237);
+	}
+
+	.sub-header {
+		font-size: 40px;
+		font-family: 'Oswald', sans-serif;
+		font-weight: 400;
+		text-transform: uppercase;
+	}
+
+	.tiny {
+		font-size: 20px;
+		font-family: 'Oswald', sans-serif;
+		color: rgb(38,183,255);
+	}
+
+	.about {
+		font-size: 25px;
+		font-family: 'Lato', sans-serif;
+	}
+
+	.color {
+		color: rgb(38,183,255);
 	}
 
 	@media (min-width: 480px) {
